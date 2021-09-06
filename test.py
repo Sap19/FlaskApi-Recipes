9d03bb1cf5ec19f5355ad64e96d29fd189a19eb6
sap19 <steven.portillo@snhu.edu>
2021-09-06 17:31:12 -0400

    Added unit test to project and requirment text
    added unit test for are api calls and requirment text doc

---
+++ test.py
@@ -0,0 +1,55 @@
+from run import app
+import json
+import unittest
+import random
+
+class FlaskTest(unittest.TestCase):
+
+	def test_recipes_get(self):
+		tester = app.test_client(self)
+		response = tester.get("recipes")
+		statusCode = response.status_code
+		self.assertEqual(statusCode, 200)
+		self.assertEqual(response.content_type, "application/json")
+
+	def test_recipes_post(self):
+		tester = app.test_client(self)
+		response = tester.post("recipes",json={
+			'name': 'butteredBagels',
+			'ingredients': ['1 bagel','butter'],
+			'instructions': ['cut the bagel','spread butter on bagel']
+		})
+		statusCode = response.status_code
+		if statusCode == 400 :
+			self.assertEqual(statusCode, 400)
+		else:
+			self.assertEqual(statusCode, 201)
+
+	def test_recipes_put(self):
+		tester = app.test_client(self)
+		response = tester.put("recipes",json={
+			'name': 'butteredBagels',
+			'ingredients': ['1 bagel','2tbs butter'],
+			'instructions': ['cut the bagel','spread butter on bagel']
+		})
+		statusCode = response.status_code
+		self.assertEqual(statusCode, 201)
+
+		response = tester.put("recipes",json={
+			'name': 'pasta',
+			'ingredients': ['1 bagel','2tbs butter'],
+			'instructions': ['cut the pasta','spread butter on pasta']
+		})
+		statusCode = response.status_code
+		self.assertEqual(statusCode, 404)
+		self.assertEqual(response.content_type, "application/json")
+
+	def test_recipes_details_get(self):
+		tester = app.test_client(self)
+		response = tester.get("recipes/details/butteredBagels")
+		statusCode = response.status_code
+		self.assertEqual(statusCode, 200)
+		self.assertEqual(response.content_type, "application/json")
+
+if __name__ == '__main__':
+	unittest.main()
